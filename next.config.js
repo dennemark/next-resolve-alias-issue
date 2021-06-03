66c32a48904ba95560019ba3429c753eaeda06eb
dennemark <martin@formfollowsyou.com>
2021-06-03 13:21:20 +0200

    init project with react-mapbox-gl and maptiler-gl

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    future: {webpack5: true},
+    reactStrictMode: true,
+    experimental: {
+      reactMode: 'concurrent'
+    },
+
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    
+      //config.resolve.alias['mapbox-gl'] =  'maplibre-gl'
+
+    config.module.rules.push({
+          resolve:{
+            alias: {
+              ...config.resolve.alias,
+              'mapbox-gl': require.resolve('maplibre-gl')
+            }
+          }
+        })
+      return config
+    }
+  } 
+  
